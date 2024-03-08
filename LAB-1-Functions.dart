ddd7f76135aa5d53c3d5915771f5f4a218131f02
beki-kel <139013988+beki-kel@users.noreply.github.com>
2024-03-08 15:51:45 +0300

    functions exercise 1 done

---
+++ LAB-1-Functions.dart
@@ -0,0 +1,10 @@
+void main() {
+  double num1 = 5.0;
+  double num2 = 7.5;
+  double sumResult = calculateSum(num1, num2);
+  print("The sum of $num1 and $num2 is: $sumResult");
+}
+
+double calculateSum(double a, double b) {
+  return a + b;
+}
