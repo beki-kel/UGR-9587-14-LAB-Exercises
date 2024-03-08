5cd50b11d9e39e086ff319f0977802f8627f8e08
beki-kel <139013988+beki-kel@users.noreply.github.com>
2024-03-08 14:45:31 +0300

    lab-1 done

---
+++ LAB-1-Variables-and-types.dart
@@ -0,0 +1,11 @@
+void main() {
+  // Create variables
+  String name = "Bereket kelay";
+  int age = 21; // Just an example age
+  String favoriteColor = "Blue";
+
+  // Print using string interpolation
+  print("Name: $name");
+  print("Age: $age");
+  print("Favorite Color: $favoriteColor");
+}
